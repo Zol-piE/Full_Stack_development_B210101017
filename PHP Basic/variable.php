19b78fa05132dd2078350c4d33bb4e7d63844b99
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-02-26 00:55:37 +0600

    Create variable.php

---
+++ PHP Basic/variable.php
@@ -0,0 +1,16 @@
+<?php
+// variable start with $
+$firstname = "tomal";
+$surname =  "islam";
+echo $firstname . " " . $surname;
+echo"<br>";
+echo "my name is $firstname<br>";
+
+$x = 10 ;
+$y = $x;
+$x = 20;
+echo "Total " . ($x+$y);
+
+
+
+?>
