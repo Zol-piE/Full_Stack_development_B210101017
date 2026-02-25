437d9c8e0a50cae4fb9e367df9742c14c5ffc309
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-02-26 00:55:32 +0600

    Create doubleSingleQuotes.php

---
+++ PHP Basic/doubleSingleQuotes.php
@@ -0,0 +1,16 @@
+<?php
+$firstname = "tomal";
+
+echo "Double quotes: my name is $firstname<br>";
+echo 'Single quotes: my name is $firstname<br>';
+
+echo "<br>";
+
+echo "Double quotes with newline: Line1\nLine2<br>";
+echo 'Single quotes with newline: Line1\nLine2<br>';
+
+echo "<br>";
+
+echo "Escape in double quotes: \"PHP\"<br>";
+echo 'Escape in single quotes: \'PHP\'<br>';
+?>
