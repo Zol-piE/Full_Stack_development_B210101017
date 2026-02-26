469fdf2156f166d0411d07644e42f84268771041
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-02-26 17:48:07 +0600

    Create dataTypes.php

---
+++ PHP Basic/dataTypes.php
@@ -0,0 +1,27 @@
+<?php
+//string
+$name = 'tomal';
+$name2 = "tomal islam";
+var_dump($name);
+echo "<br>";
+echo "name2 get type " . gettype($name2);
+echo "<br>";
+
+//interger
+$num1 = 10;
+$num2 = -20;
+var_dump($num1,$num2);
+echo "<br>";
+
+//float 
+$f1 = 1.2;
+var_dump(($f1));
+echo "<br>";
+//array
+
+$arr = array('tomal','islam','tohidul');
+print_r($arr);
+echo "<br>";
+var_dump($arr);
+echo "<br>";
+?>
