417200b2e6b19c3f7e96f1ea77a526da78215ae0
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-05-05 21:19:28 +0600

    Update script.js

--- Java Script Fundamentals/script.js
+++ Java Script Fundamentals/script.js
@@ -17,3 +17,9 @@ let mark = 72;
 if (mark >= 80) console.log(`A+`);
 else if (mark >= 70) console.log(`A`);
 else console.log(`fail`);
+console.log(true + true);
+// truthy and falsy value
+// (0,null,'',undefined,Nan)) are falsy values
+let height;
+if (height) console.log(`My height is ${height}`);
+else console.log(height);
