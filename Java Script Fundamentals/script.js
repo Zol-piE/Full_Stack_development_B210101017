0278fb1e0123a06a7276b1a0762ca63d3d0cac18
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-05-05 19:28:41 +0600

    Create script.js

---
+++ Java Script Fundamentals/script.js
@@ -0,0 +1,14 @@
+let js = "working";
+if (js == "working") alert("Hello World");
+else alert("World not Working");
+js = 2026;
+console.log(js);
+
+// Working with string
+let firstName = "Tomal";
+console.log(firstName);
+let lastName = "islam";
+console.log(firstName + " " + lastName);
+// template Literals
+let str = `hello , i am ${firstName} ${lastName} `;
+console.log(str);
