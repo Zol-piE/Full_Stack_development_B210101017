c12c92d04afdc3291795b871f916ac3649234766
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-05-12 16:11:37 +0600

    Update script.js

--- JS DOM & Events/script.js
+++ JS DOM & Events/script.js
@@ -1,8 +1,24 @@
 "use strict";
-let x = document.querySelector(".GuessLayout");
-x.style.setProperty("--symbol", '"13"');
+
 const button = document.querySelector(".click");
 
+let randomNumber = Math.trunc(Math.random() * 6) + 1;
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") button.click();
+});
+
 button.addEventListener("click", () => {
-  console.log(document.querySelector("#inputNumber").value);
+  let guess = Number(document.querySelector("#inputNumber").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "❌ Not Number";
+  } else if (guess === randomNumber) {
+    document.querySelector(".message").textContent = "🎉 Congrats";
+
+    let x = document.querySelector(".GuessLayout");
+
+    x.style.setProperty("--symbol", `"${randomNumber}"`);
+  } else {
+    document.querySelector(".message").textContent = `😒 HUH! Not ${guess}`;
+  }
 });
