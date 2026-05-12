293d7ae9440da999c65321a806576ebebf49ea03
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-05-12 16:25:28 +0600

    Update script.js

--- JS DOM & Events/script.js
+++ JS DOM & Events/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let score = 1;
 const button = document.querySelector(".click");
 
 let randomNumber = Math.trunc(Math.random() * 6) + 1;
@@ -10,15 +11,21 @@ document.addEventListener("keydown", (e) => {
 button.addEventListener("click", () => {
   let guess = Number(document.querySelector("#inputNumber").value);
 
-  if (!guess) {
-    document.querySelector(".message").textContent = "❌ Not Number";
-  } else if (guess === randomNumber) {
-    document.querySelector(".message").textContent = "🎉 Congrats";
-
-    let x = document.querySelector(".GuessLayout");
-
-    x.style.setProperty("--symbol", `"${randomNumber}"`);
-  } else {
-    document.querySelector(".message").textContent = `😒 HUH! Not ${guess}`;
-  }
+  if (score >= 1) {
+    if (!guess) {
+      document.querySelector(".message").textContent = "❌ Not Number";
+    } else if (guess === randomNumber) {
+      document.querySelector(".message").textContent = "🎉 Congrats";
+      let x = document.querySelector(".GuessLayout");
+      x.style.setProperty("--symbol", `"${randomNumber}"`);
+    } else if (guess > randomNumber) {
+      document.querySelector(".message").textContent = `📈 Too High`;
+      score--;
+    } else {
+      document.querySelector(".message").textContent = `📉 Too Low`;
+      score--;
+    }
+  } else
+    document.querySelector(".message").textContent = `😵 You Lose the game`;
+  document.querySelector(".score").textContent = score;
 });
